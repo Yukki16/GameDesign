592ed603c02b83992af1b292c651b8ec3d439f70
Yukki16 <89789213+Yukki16@users.noreply.github.com>
2022-01-04 15:37:50 +0100

    Spikes?

---
+++ GXPEngine2022BB/GXPEngine/Spike.cs
@@ -0,0 +1,17 @@
+﻿using GXPEngine.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TiledMapParser;
+
+namespace GXPEngine
+{
+    class Spike : Sprite
+    {
+        public Spike(TiledObject obj) : base(new Texture2D(16, 16))
+        {
+            //comment
+        }
+    }
+}
